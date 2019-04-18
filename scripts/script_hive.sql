d06274362392dbff92629402ea52093d12280e0b
huertaa <47197840+huertaa@users.noreply.github.com>
2019-04-18 09:43:27 -0400

    Add files via upload

--- scripts/script_hive.sql
+++ scripts/script_hive.sql
@@ -13,18 +13,18 @@ $ hdfs dfs -put routes.dat routes
 CREATE EXTERNAL TABLE IF NOT EXISTS Airports_dat(
 AirportID INT, AirportName STRING, City STRING,
 Country STRING, IATA STRING, ICAO STRING, Latitude DOUBLE, Longitude DOUBLE, Altitude INT, 
-Timezone DOUBLE, DST CHAR, Tz STRING, Type STRING, Source STRING)
+Timezone DOUBLE, DST CHAR(10), Tz STRING, AirportType STRING, Source STRING)
 COMMENT 'Airports'
 ROW FORMAT DELIMITED
 FIELDS TERMINATED BY ','
 STORED AS TEXTFILE
-LOCATION '/tmp/openflight';
+LOCATION '/tmp/openflight/database';
 
 
 CREATE TABLE IF NOT EXISTS Airports(
 AirportID INT, AirportName STRING, City STRING,
 Country STRING, IATA STRING, ICAO STRING, Latitude DOUBLE, Longitude DOUBLE, Altitude INT,
-Timezone DOUBLE, DST CHAR, Tz STRING, Type STRING, Source STRING)    
+Timezone DOUBLE, DST CHAR(10), Tz STRING, AirportType STRING, Source STRING)    
 COMMENT 'Airports'
 STORED AS ORC;
 
@@ -34,17 +34,17 @@ INSERT OVERWRITE TABLE Airports SELECT * FROM Airports_dat;
 
 CREATE EXTERNAL TABLE IF NOT EXISTS Airlines_dat(
 AirlineID INT, AirlineName STRING, Alias STRING,
-IATA STRING, ICAO STRING, Callsign STRING, Country STRING, Active CHAR
+IATA STRING, ICAO STRING, Callsign STRING, Country STRING, Active CHAR(10)
 COMMENT 'Airlines'
 ROW FORMAT DELIMITED
 FIELDS TERMINATED BY ','
 STORED AS TEXTFILE
-LOCATION '/tmp/openflight';
+LOCATION '/tmp/openflight/database';
 
 
 CREATE TABLE IF NOT EXISTS Airlines(
 AirlineID INT, AirlineName STRING, Alias STRING,
-IATA STRING, ICAO STRING, Callsign STRING, Country STRING, Active CHAR
+IATA STRING, ICAO STRING, Callsign STRING, Country STRING, Active CHAR(10)
 COMMENT 'Airlines'   
 STORED AS ORC;
 
@@ -59,7 +59,7 @@ COMMENT 'Plane'
 ROW FORMAT DELIMITED
 FIELDS TERMINATED BY ','
 STORED AS TEXTFILE
-LOCATION '/tmp/openflight';
+LOCATION '/tmp/openflight/database';
 
 CREATE TABLE IF NOT EXISTS Planes(
 PlaneName STRING, IATA STRING, ICAO STRING   
@@ -72,17 +72,17 @@ INSERT OVERWRITE TABLE Planes SELECT * FROM Planes_dat;
 
 CREATE EXTERNAL TABLE IF NOT EXISTS Routes_dat(
 Airline STRING, AirlineID INT, 
-Destination STRING, DestinationID INT, Codeshare CHAR, Stops INT, Equipment STRING
+Destination STRING, DestinationID INT, Codeshare CHAR(10), Stops INT, Equipment STRING
 COMMENT 'Routes'
 ROW FORMAT DELIMITED
 FIELDS TERMINATED BY ','
 STORED AS TEXTFILE
-LOCATION '/tmp/openflight';
+LOCATION '/tmp/openflight/database';
 
 
 CREATE TABLE IF NOT EXISTS Routes(
 Airline STRING, AirlineID INT, 
-Destination STRING, DestinationID INT, Codeshare CHAR, Stops INT, Equipment STRING
+Destination STRING, DestinationID INT, Codeshare CHAR(10), Stops INT, Equipment STRING
 COMMENT 'Routes'
 STORED AS ORC;
 
