bd3f29eda5102ff6e273804cdf79c8ed6c58fbbe
Nihaal George <georgenp@rose-hulman.edu>
2019-05-02 13:35:21 -0400

    shell scripts

--- sqoopscripts/mostAirportConnections.sh
+++ sqoopscripts/mostAirportConnections.sh
@@ -3,6 +3,4 @@ USE OpenAnalysis;
 DROP TABLE IF EXISTS MostConnectedAirports;
 CREATE TABLE MostConnectedAirports(airportID int, airportName varchar(50), connections int, UNIQUE KEY duplicateConstraint(airportID));"
 
-sqoop export --connect jdbc:mysql://10.128.0.4:3306/OpenAnalysis --username hive --password admin -m 1 --table MostConnectedAirports --export-dir /tmp/openflight/output/mostConnectedAirports/part-r-00000 --input-fields-terminated-by '\t' --input-null-string 'NULL String' --input-null-non-string '\\N' --update-key airportID --update-mode allowinsert
-
 exit 0;
