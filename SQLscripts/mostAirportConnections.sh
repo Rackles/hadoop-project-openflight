7cea8a317d34a4566143e0ce8f6ecf3c0d1e184d
Nihaal George <georgenp@rose-hulman.edu>
2019-05-02 23:13:39 -0400

    Made SQL scripts

--- SQLscripts/mostAirportConnections.sh
+++
@@ -1,6 +0,0 @@
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS OpenAnalysis;
-USE OpenAnalysis;
-DROP TABLE IF EXISTS MostConnectedAirports;
-CREATE TABLE MostConnectedAirports(airportID int, airportName varchar(150), connections int);"
-
-exit 0;
