9f77973ae5f8397bfd6f514a788df4ffe5d3019e
Nihaal George <georgenp@rose-hulman.edu>
2019-05-06 14:39:12 -0400

    lab 7

--- georgenp/lab7/currentTask.sh
+++ georgenp/lab7/currentTask.sh
@@ -1,3 +1,3 @@
-sqoop import-all-tables --connect jdbc:mysql://$1/sqooptest --username hive --password admin -m 1 --warehouse-dir /tmp/sqoopAll/ --fields-terminated-by ',' --null-string '\\N' --null-non-string '\\N'
+sqoop import-all-tables --connect jdbc:mysql://$1/georgenpsqoop --username hive --password admin -m 1 --warehouse-dir /tmp/sqoopAll/ --fields-terminated-by ',' --null-string '\\N' --null-non-string '\\N'
 
 exit 0;
