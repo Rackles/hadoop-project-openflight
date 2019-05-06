09677fe98ffa8dec483c1f1bfff91133b8ee368a
Nihaal George <georgenp@rose-hulman.edu>
2019-05-06 00:26:07 -0400

    lab 7

---
+++ georgenp/lab7/task1georgenp.sh
@@ -0,0 +1,11 @@
+sqoop import --connect jdbc:mysql://$1/georgenpsqoop --username hive --password admin --table Employees --split-by eid --target-dir /tmp/sqoopOutput
+
+sqoop import --connect jdbc:mysql://$1/georgenpsqoop --username hive --password admin --table Employees -m 1 --target-dir /tmp/sqoopMapOutput
+
+sqoop import --connect jdbc:mysql://$1/georgenpsqoop --username hive --password admin --table Employees -m 2 --target-dir /tmp/sqoopSeqOutput --as-sequencefile
+
+sqoop import --connect jdbc:mysql://$1/georgenpsqoop --username hive --password admin --table Employees -m 2 --warehouse-dir /tmp/sqoop/Employees
+
+sqoop import --connect jdbc:mysql://$1/georgenpsqoop --username hive --password admin --table Employees -m 2 --warehouse-dir /tmp/sqoop/Employees --fields-terminated-by '\t' --null-string 'This is a Null String'
+
+exit 0;
