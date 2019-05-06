31c0e5bb8ba873b94a318ae2a19480c7e776f85a
Nihaal George <georgenp@rose-hulman.edu>
2019-05-06 14:17:00 -0400

    lab 7

--- georgenp/lab7/currentTask.sh
+++ georgenp/lab7/currentTask.sh
@@ -1,3 +1,3 @@
-sqoop import --connect jdbc:mysql://$1/georgenpsqoop --username hive --password admin --table Employees -m 2 --split-by eid --warehouse-dir /tmp/sqoop/Employees --fields-terminated-by '\t' --null-string 'This is a Null String'
+sqoop import --connect jdbc:mysql://$1/georgenpsqoop --username hive --password admin --table Employees -m 2 --split-by eid --warehouse-dir /tmp/sqoop/Employees2 --fields-terminated-by '\t' --null-string 'This is a Null String'
 
 exit 0;
