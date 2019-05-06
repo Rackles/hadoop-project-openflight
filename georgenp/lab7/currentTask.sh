1fa4bcb7575dd3ab11f500e304633ec75a4c0d09
Nihaal George <georgenp@rose-hulman.edu>
2019-05-06 14:30:17 -0400

    lab 7

--- georgenp/lab7/currentTask.sh
+++ georgenp/lab7/currentTask.sh
@@ -1,3 +1,3 @@
-sqoop create-hive-table --connect jdbc:mysql://$1/georgenpsqoop --username hive --password admin -m 2 --table Employees --split-by eid --hive-import --create-hive-table --hive-table sqooptest.Employees
+sqoop import --connect jdbc:mysql://$1/georgenpsqoop --username hive --password admin -m 2 --table Employees --split-by eid --hive-import --create-hive-table --hive-table sqooptest.Employees
 
 exit 0;
