0332c6603a000d3fffa5f6060ce2493ee27c48e5
Sebastian Harner <s.harner@web.de>
2019-05-09 20:46:22 -0400

    Change sql database

--- sqoopscripts/mostConnectedAirportSqoop.sh
+++ sqoopscripts/mostConnectedAirportSqoop.sh
@@ -1,3 +1,3 @@
-sqoop export --connect jdbc:mysql://10.128.0.4:3306/OpenAnalysis --username hive --password admin -m 1 --table MostConnectedAirport --export-dir /tmp/openflight/output/mostConnectedAirports/part-r-00000 --input-fields-terminated-by '\t' --input-null-string '\\N' --input-null-non-string '\\N'
+sqoop export --connect jdbc:mysql://193.196.37.32:3306/OpenAnalysis --username root --password admin -m 1 --table MostConnectedAirport --export-dir /tmp/openflight/output/mostConnectedAirports/part-r-00000 --input-fields-terminated-by '\t' --input-null-string '\\N' --input-null-non-string '\\N'
 
 exit 0;
