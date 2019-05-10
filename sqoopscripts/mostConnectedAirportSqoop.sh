b5ae126247ae92099b23c381df2f651d97e589bf
Sebastian Harner <s.harner@web.de>
2019-05-09 21:47:49 -0400

    Switch to postgres

--- sqoopscripts/mostConnectedAirportSqoop.sh
+++ sqoopscripts/mostConnectedAirportSqoop.sh
@@ -1,3 +1,3 @@
-sqoop export --connect jdbc:mysql://193.196.37.32:3306/OpenAnalysis --username root --password admin -m 1 --table MostConnectedAirport --export-dir /tmp/openflight/output/mostConnectedAirports/part-r-00000 --input-fields-terminated-by '\t' --input-null-string '\\N' --input-null-non-string '\\N'
+sqoop export --connect jdbc:postgresql://193.196.37.32:5432/openanalysis --username postgres --password admin -m 1 --table MostConnectedAirport --export-dir /tmp/openflight/output/mostConnectedAirports/part-r-00000 --input-fields-terminated-by '\t' --input-null-string '\\N' --input-null-non-string '\\N'
 
 exit 0;
