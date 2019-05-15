70d1498b9fda5eabfd3f83d3c92e7b8293b0b5ba
Nihaal George <georgenp@rose-hulman.edu>
2019-05-15 18:43:23 -0400

    oozie take 1

--- SQLscripts/highAltitudesSQL.sh
+++ SQLscripts/highAltitudesSQL.sh
@@ -1,4 +1,4 @@
-psql -h 193.196.37.32 -p 5432 -d openanalysis -U postgres -w -e "CREATE DATABASE IF NOT EXISTS OpenAnalysis;
+psql -h 193.196.37.32 -p 5432 -d openanalysis -U postgres -w -c "CREATE DATABASE IF NOT EXISTS OpenAnalysis;
 USE OpenAnalysis;
 DROP TABLE IF EXISTS HighAltitudes2;
 CREATE TABLE HighAltitudes2(id varchar(10), name varchar(150), city varchar(50), country varchar(50), iata varchar(10), alt decimal, latitude decimal, longitude decimal);"
